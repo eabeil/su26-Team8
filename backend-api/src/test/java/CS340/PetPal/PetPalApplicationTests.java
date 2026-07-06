dcba0a9e4f9add65139c773d6657ee930a838819
Emmanuel Abeil <e_abeil@uncg.edu>
2026-07-06 10:29:07 -0400

    ADDED: backend files

---
+++ backend-api/src/test/java/CS340/PetPal/PetPalApplicationTests.java
@@ -0,0 +1,13 @@
+package CS340.PetPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
