53235ed83189968fe0d2c07591f9be905887a3ab
Emmanuel Abeil <e_abeil@uncg.edu>
2026-07-07 22:29:50 -0400

    added repo files

---
+++ backend-api/src/main/java/CS340/PetPal/Repository/CustomerRepository.java
@@ -0,0 +1,15 @@
+package CS340.PetPal.Repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import CS340.PetPal.Entity.Customer;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    
+
+    Optional<Customer> findByEmail(String email);
+}
---
+++ backend-api/src/main/java/CS340/PetPal/Repository/PetRepository.java
@@ -0,0 +1,14 @@
+package CS340.PetPal.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import CS340.PetPal.Entity.Pet;
+
+@Repository
+public interface PetRepository extends JpaRepository<Pet, Long> {
+    
+    List<Pet> findByCustomerId(Long customerId);
+}
