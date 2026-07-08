dd7291d0f45ec9d5a2c290385d55d534447a5d70
Emmanuel Abeil <e_abeil@uncg.edu>
2026-07-07 21:18:44 -0400

    removed collections

--- backend-api/src/main/java/CS340/PetPal/Entity/Pet.java
+++ backend-api/src/main/java/CS340/PetPal/Entity/Pet.java
@@ -4,9 +4,8 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CollectionTable;
+
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
