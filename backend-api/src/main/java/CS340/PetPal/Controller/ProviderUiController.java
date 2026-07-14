a359aca0b86daf62c5d39b2704d613b938e3e9cb
jmandevel <newjourneyman@yahoo.com>
2026-07-14 16:11:22 -0400

    Remove AllArgsConstructor from ProviderUiController
    There are no args why is this here idk what is life baby dont hurt me no more

--- backend-api/src/main/java/CS340/PetPal/Controller/ProviderUiController.java
+++ backend-api/src/main/java/CS340/PetPal/Controller/ProviderUiController.java
@@ -3,11 +3,8 @@ package CS340.PetPal.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import lombok.AllArgsConstructor;
-
 @Controller
 @RequestMapping("/provider")
-@AllArgsConstructor
 public class ProviderUiController {
     // TODO pages for provider go here
     // job for jmandevel
