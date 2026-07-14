ef099478257aafe49095d1be5ea2c782eb478afa
jmandevel <newjourneyman@yahoo.com>
2026-07-13 22:25:11 -0400

    Add UI controllers for customer and provider
    Splitting the pages into two different UI controllers to make it easier to collaborate.

---
+++ backend-api/src/main/java/CS340/PetPal/Controller/CustomerUiController.java
@@ -0,0 +1,14 @@
+package CS340.PetPal.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/customer")
+@AllArgsConstructor
+public class CustomerUiController {
+    // TODO pages for customer go here
+    // job for eabeil
+}
