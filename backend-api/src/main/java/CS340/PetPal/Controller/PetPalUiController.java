ba0d4c62949f7d67ec45144bc4cd979d23e29c9f
jmandevel <newjourneyman@yahoo.com>
2026-07-13 22:22:01 -0400

    Add login page endpoint
    Add endpoint to load login page using Apache FreeMarker

---
+++ backend-api/src/main/java/CS340/PetPal/Controller/PetPalUiController.java
@@ -0,0 +1,14 @@
+package CS340.PetPal.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class PetPalUiController {
+    @GetMapping({"", "/"})
+    public String homePage() {
+        return "login-page.ftlh";
+    }
+}
