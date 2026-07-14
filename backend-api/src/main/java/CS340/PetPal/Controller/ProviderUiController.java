85aa988c1a2c63b1e1927bcae5e57c4250c6fdb0
jmandevel <newjourneyman@yahoo.com>
2026-07-14 16:44:36 -0400

    Fix provider id in urls

--- backend-api/src/main/java/CS340/PetPal/Controller/ProviderUiController.java
+++ backend-api/src/main/java/CS340/PetPal/Controller/ProviderUiController.java
@@ -1,12 +1,13 @@
 package CS340.PetPal.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/provider/${providerId}")
+@RequestMapping("/provider/{providerId}")
 public class ProviderUiController {
 
    @GetMapping({"/", ""})
@@ -15,42 +16,50 @@ public class ProviderUiController {
    }
 
     @GetMapping({"/customer-profile", "/customer-profile/"})
-    public String customerProfile(@PathVariable Long providerId) {
+    public String customerProfile(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/customer-profile";
     }
 
     @GetMapping({"/dashboard", "/dashboard/"})
-    public String dashboard(@PathVariable Long providerId) {
+    public String dashboard(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/dashboard";
     }
 
     @GetMapping({"/edit-contact", "/edit-contact/"})
-    public String editContact(@PathVariable Long providerId) {
+    public String editContact(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/edit-contact";
     }
 
     @GetMapping({"/edit-profile", "/edit-profile/"})
-    public String editProfile(@PathVariable Long providerId) {
+    public String editProfile(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/edit-profile";
     }
 
     @GetMapping({"/edit-services", "/edit-services"})
-    public String editServices(@PathVariable Long providerId) {
+    public String editServices(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/edit-services";
     }
 
     @GetMapping({"/pet-profile", "/pet-profile/"})
-    public String petProfile(@PathVariable Long providerId) {
+    public String petProfile(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/pet-profile";
     }
 
     @GetMapping({"/respond-reviews", "/respond-reviews/"})
-    public String respondReviews(@PathVariable Long providerId) {
+    public String respondReviews(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/respond-reviews";
     }
 
     @GetMapping({"/search-customers", "/search-customers/"})
-    public String searchCustomers(@PathVariable Long providerId) {
+    public String searchCustomers(@PathVariable Long providerId, Model model) {
+        model.addAttribute("providerId", providerId);
         return "provider/search-customers";
     }
 }
