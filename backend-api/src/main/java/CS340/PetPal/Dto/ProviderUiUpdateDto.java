cf228aad218c5b316abbe7f5c1c2f46ff881d73f
Dan Valcour <newjourneyman@yahoo.com>
2026-07-17 15:15:35 -0400

    Add provider ui update dto

---
+++ backend-api/src/main/java/CS340/PetPal/Dto/ProviderUiUpdateDto.java
@@ -0,0 +1,19 @@
+package CS340.PetPal.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProviderUiUpdateDto {
+    private String name;
+    private String description;
+    private String imageUrl;
+    private String address;
+    private String phone;
+    private String email;
+}
