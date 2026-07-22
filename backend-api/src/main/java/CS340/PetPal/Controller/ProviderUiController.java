6bcdae297a905f143d3ab7f827ad4406a02c106d
Dan Valcour <newjourneyman@yahoo.com>
2026-07-21 20:30:37 -0400

    Revert "Add missing password encode"
    This reverts commit c93ae16e94121fbdecd9d8ed608bafdc1b508297.

--- backend-api/src/main/java/CS340/PetPal/Controller/ProviderUiController.java
+++ backend-api/src/main/java/CS340/PetPal/Controller/ProviderUiController.java
@@ -317,9 +317,8 @@ public class ProviderUiController {
             redirectAttributes.addAttribute("description", dto.getDescription());
             return ProviderUiController.getRedirect(providerId, "profile-edit") + "#scrolly";
         }
-        String encodedPassword = this.passwordEncoder.encode(dto.getPassword());
         ProviderUpdateDto update_dto = new ProviderUpdateDto(dto.getName().trim(), dto.getDescription().trim(), dto.getImageUrl().trim(),
-                dto.getAddress().trim(), dto.getPhone().trim(), dto.getEmail().trim(), encodedPassword);
+                dto.getAddress().trim(), dto.getPhone().trim(), dto.getEmail().trim(), dto.getPassword());
         this.providerService.updateProvider(providerId, update_dto);
         return ProviderUiController.getRedirect(providerId, "dashboard");
     }
