bbe6efd2c3818b282402cd97cb34cb569b32446d
jmandevel <newjourneyman@yahoo.com>
2026-07-22 16:28:22 -0400

    Remove unecessary dto from java file

--- backend-api/src/main/java/CS340/PetPal/Controller/PetPalUiController.java
+++ backend-api/src/main/java/CS340/PetPal/Controller/PetPalUiController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import CS340.PetPal.Dto.ProviderSignupDto;
 import CS340.PetPal.Service.ProviderService;
 import jakarta.servlet.http.HttpSession;
 
@@ -17,8 +16,6 @@ public class PetPalUiController {
     @GetMapping({"", "/"})
     public String homePage(Model model, HttpSession session) {
         session.invalidate();
-        ProviderSignupDto dto = new ProviderSignupDto();
-        model.addAttribute("dto", dto);
         return "login-page";
     }
     
